bb69741f704ef23d90227ef7050074f9df993765
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-02-21 02:02:53 +0500

    34 Exercise

---
+++ 34-Exercise/app.ts
@@ -0,0 +1,10 @@
+// Think of at least three kinds of your favorite pizza. Store these pizza names in an array, and
+// then use a for loop to print the name of each pizza.
+// • Modify your loop to print a sentence using the name of the pizza instead of printing just the name of
+// the pizza. For each pizza you should have one line of output containing a simple statement like I like
+// pepperoni pizza
+
+let favoritePizzas = ["pepperoni", "margherita", "hawaiian"];
+for (let i = 0; i < favoritePizzas.length; i++) {
+    console.log("I like " + favoritePizzas[i] + " pizza");
+}
