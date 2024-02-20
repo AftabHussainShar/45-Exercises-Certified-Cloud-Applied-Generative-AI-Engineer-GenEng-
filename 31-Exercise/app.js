44db63f7974fc26286e68d0d663fe868254f7cd3
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-02-21 01:55:29 +0500

    31 Exsercise

---
+++ 31-Exercise/app.js
@@ -0,0 +1,12 @@
+// Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed
+var users = ['admin', 'eric', 'john', 'jane', 'mark'];
+if (users.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    // Remove all usernames from the array
+    users = [];
+    console.log('Usernames removed. The list is now empty.');
+}
