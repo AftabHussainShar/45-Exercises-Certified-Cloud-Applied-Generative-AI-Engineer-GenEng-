dc01b49f0ae97d4f8600fff0f90ec4a578ba8d79
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-02-21 02:18:28 +0500

    41 Exsercise

---
+++ 41-Exercise/app.js
@@ -0,0 +1,9 @@
+// Make an array of magician’s names. Pass the array to a function called show_magicians(), which
+// prints the name of each magician in the array.
+var magicians = ["Harry Houdini", "David Blaine", "Teller", "Penn Jillette"];
+function show_magicians(magicians) {
+    for (var i = 0; i < magicians.length; i++) {
+        console.log(magicians[i]);
+    }
+}
+show_magicians(magicians);
---
+++ 41-Exercise/app.ts
@@ -0,0 +1,10 @@
+// Make an array of magician’s names. Pass the array to a function called show_magicians(), which
+// prints the name of each magician in the array.
+
+let magicians: string[] = ["Harry Houdini", "David Blaine", "Teller", "Penn Jillette"];
+function show_magicians(magicians: string[]) {
+    for (let i = 0; i < magicians.length; i++) {
+        console.log(magicians[i]);
+    }
+}
+show_magicians(magicians);
