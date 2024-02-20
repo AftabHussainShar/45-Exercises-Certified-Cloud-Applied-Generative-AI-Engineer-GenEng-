8aa55056c0c82600b1c9aa1eed25c6bf51233fd8
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-02-21 02:12:40 +0500

    37 Exercise

---
+++ 37-Exercise/app.js
@@ -0,0 +1,15 @@
+// Modify the make_shirt() function so that shirts are large by default, with a message that reads
+// I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any
+// size with a different message
+function make_shirt(size, message) {
+    if (size === void 0) { size = "large"; }
+    if (size === "large") {
+        console.log("The size of the shirt is ".concat(size, " and  ").concat(message, "."));
+    }
+    else {
+        console.log("The size of the shirt is ".concat(size, " and I will Not wear it and").concat(message, "."));
+    }
+}
+make_shirt("large", "I love Typescript");
+make_shirt("medium", "I love Typescript");
+make_shirt("small", "I love Typescript");
